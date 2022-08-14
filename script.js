a796fadccad942cae978240dd352ccc69a395a55
Alexandra Mosyagina <mosyagina.alexandra@mail.ru>
2022-08-15 00:34:07 +0700

    i did it

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ function compute(){
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+     //check if a principle is zero or less than zero
+     if(principal == undefined || principal <= 0){
+        alert("Please, enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    //count and display
+    else{
+        var user_deposit = "If you deposit <mark>" + principal+",</mark><br>";
+        var user_rate = "at an interest rate of <mark>" + rate+"%,</mark><br>";
+        var user_interest = "You will receive an amount of <mark>"+ interest+",</mark><br>";
+        var user_year = "in the year <mark'>" + year +"</mark>";
+
+        document.getElementById("result").innerHTML=user_deposit+user_rate+user_interest+user_year;
+    }
 }
+//update a rate
 function updateRate(){
         var rateval = document.getElementById("rate").value;
         document.getElementById("rate_val").innerText=rateval;
