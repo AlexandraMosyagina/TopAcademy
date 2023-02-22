124ac36c6b8eb7f26e93151dac65b1496ef23122
Alexandra Mosyagina <mosyagina.alexandra@mail.ru>
2023-02-23 01:32:25 +0700

    This program finds words and replaces for another.

---
+++ homeworks/replaceWord/hw4.py
@@ -0,0 +1,8 @@
+# the fifth task
+
+userString = input("Please, enter something\n")
+userWord = input("Please, enter the replacement word\n")
+userReplace = input("Please, enter the word for replace\n")
+editedString = userString.replace(userWord, userReplace)
+
+print(f"{editedString}")
