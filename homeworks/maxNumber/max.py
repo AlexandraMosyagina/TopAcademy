59e903517e04dce3683ac8d6bb1e0d920048e0e3
Alexandra Mosyagina <mosyagina.alexandra@mail.ru>
2023-02-23 01:27:19 +0700

    This program finds max number.

---
+++ homeworks/maxNumber/max.py
@@ -0,0 +1,6 @@
+"""This is a first homework on Python."""
+# the second task
+firstNumber = int(input("Enter a first number"))
+secondNumber = int(input("Enter a second number"))
+thirdNumber = int(input("Enter a third number"))
+print(max(firstNumber, secondNumber, thirdNumber))
